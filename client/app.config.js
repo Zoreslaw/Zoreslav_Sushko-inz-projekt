f9144f06c030f45780c894241ed0331ffd78e82a
Zoreslaw <92851656+Zoreslaw@users.noreply.github.com>
2025-12-05 15:39:33 +0100

    Fix: Point googleServicesFile to allowed path in app.config.js

--- client/app.config.js
+++ client/app.config.js
@@ -31,7 +31,7 @@ export default {
         // foregroundImage: "./assets/images/book_icon.png",
         backgroundColor: "#232323",
       },
-      googleServicesFile: "./configs/google-services.json",
+      googleServicesFile: "./android/app/google-services.json",
       package: "com.umaps.teamup",
       package_name: "com.umaps.teamup",
     },
